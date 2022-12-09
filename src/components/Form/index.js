df5de088135ca4b9802ef65aa5c67d36781fe1a8
TiagoUniverse <tiagocesar68@gmail.com>
2022-12-08 22:41:16 -0300

    Aplicativo de IMC

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,30 +1,62 @@
-import React from "react"
+import React , {useState} from "react"
 import { View, Text } from "react-native"
 import { Button, TextInput } from "react-native-web";
-
+import ResultImc from "./ResultImc";
 
 
 export default function Form() {
+
+    const [height, setHeight]= useState(null)
+    const [weight, setWeight]= useState(null)
+    const [messageIMC, setMessageImc]= useState("preencha o peso e altura")
+    const [imc , setImc]= useState(null)
+    const [textButton , setTextButton]= useState("Calcular")
+
+    function imcCalculator(){
+        return setImc((weight /(height*height)).toFixed(2))
+    }
+
+    function validationImc(){
+        if (weight != null && height != null){
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu imc é igual:")
+            setTextButton("Calcular novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("preencha o peso e altura")
+    }
+
     return (
         <View>
             <View>
                 <Text>Altura</Text>
                 <TextInput
+                    onChangeText={setHeight}
+                    value={height}
                     placeholder="Ex. 1.70"
                     KeyboardType="numeric"
                 />
 
                 <Text>Peso</Text>
                 <TextInput
+                    onChangeText={setWeight}
+                    value={weight}
                     placeholder="Ex. 60.30"
-                    KeyboardType="numeric" 
+                    KeyboardType="numeric"
                 />
 
-                <Button title="Calcular IMC"/>
+                <Button 
+                    title="Calcular IMC" 
+                    onPress={() => validationImc()}
+                    />
             </View>
 
+            <ResultImc messageResultImc={messageIMC} ResultImc={imc} />
 
-            <View></View>
         </View>
     );
 }
